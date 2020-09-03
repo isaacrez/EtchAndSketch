f17d357ae0d44d0bf8ea6d8b921d5c469794692f
isaacrez <isaacrez20@gmail.com>
2020-09-03 15:28:26 -0400

    Add reset functionality

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 
 function populateGrid(gridSize) {
     let content = document.querySelector('#content');
+    removeAllChildren(content);
     content.style.gridTemplate = `repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr)`;
 
     for (let x = 0; x < gridSize; x++) {
         for (let y = 0; y < gridSize; y++) {
             let cell = document.createElement('div');
-            cell.classList.add('cell');
+            cell.addEventListener('mouseover', colorCell);
             content.appendChild(cell);
         }
     }
 }
 
-populateGrid(32);
+function removeAllChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function colorCell(e) {
+    this.style.backgroundColor = 'white';
+}
+
+const gridSize = document.querySelector('#gridSize');
+const reset = document.querySelector('#reset');
+
+reset.addEventListener('click', (e) => {populateGrid(gridSize.value)});
+window.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+        populateGrid(gridSize.value);
+    }
+})
+
+populateGrid(gridSize.value);
